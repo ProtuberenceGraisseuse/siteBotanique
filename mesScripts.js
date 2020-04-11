23ceae11210a2f47868af10a37e7d8d150546d02
Nicophile <nicophile@gmail.com>
2020-04-11 08:42:49 -0400

    first commit

---
+++ mesScripts.js
@@ -0,0 +1,50 @@
+function myFunction() {
+    var clade = document.getElementById("panel") ;
+
+    if (clade.style.display === "none"){
+        clade.style.display = "block";
+    } else {
+        clade.style.display = "none" ;
+    }
+}
+
+//const echantillonBase = [
+//["nombre común","Especie","genero","familia","ordre","classe","Arboles","interes medicinale","arbustos","peligro","fibras cesteria"],
+//["helecho pelicula","hymenophyllum cruentum","hymenophyllum","hymenophyllaceae","polypodiales","polypodiopsida","0","","","",""],
+//["helecho pelicula","hymenophyllum caudiculatum","hymenophyllum","hymenophyllaceae","polypodiales","polypodiopsida","0","","","",""],
+//];
+
+ document.getElementById("#firstLevel").color = blue;
+
+
+// document.getElementById("firstLevel").innerHTML = "<p>blarf</p>";
+
+// const montrerClade = (argument) => {
+//     const cladeToShow = argument;
+//     var 
+// }
+
+// click sur "clade" --> affiche le "clade" à gauche et le(s) sous-clade à droite
+// function navigation() {
+
+//     // pour l'importation du tableau voire https://www.papaparse.com/
+
+//     var nomClade = 1 ; // find name of clicked clade
+//     var cladeLevel = 1 ; // find the level of the clicked clade
+    
+//     if (cladeLevel !== Especie) {
+//         //first identify the lines in database
+//         // make an array with the line numbers of clicked item
+//         // erase doublets (may be part of previous step ?)
+//         var numSousClade = 1 ; //number of different under-clade
+//         // make an array of 2 vertical divs.
+//         // first division : 
+//             // first line "clade name" vertical 
+//             // second line "short clade description" horizontal with border
+//         // second division : under-clades
+//             // each line is a description of one under-clade
+//     } else (cladeLevel === Especie) {
+//         var SousClade = "";// Show specie description on the right part of the screen
+//     }
+
+// }
